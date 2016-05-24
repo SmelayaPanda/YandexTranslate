3e82eccce979d02e8965fee814c3efbe4222a523
SmelayaPanda <smelayapanda@mail.ru>
2016-05-25 00:48:28 +0600

    350 000 word handling

--- src/main/java/start/StartYandexTranslate.java
+++ src/main/java/start/StartYandexTranslate.java
@@ -1,8 +1,10 @@
 package start;
 
+import ytrans.ApiKeys;
 import ytrans.Language;
 import ytrans.Translate;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,34 +14,35 @@ public class StartYandexTranslate {
 
         List<WordUnit> list = new ArrayList();
 
+        Translate.setKey(ApiKeys.YANDEX_API_KEY);
         try {
             Translate.execute("from", Language.ENGLISH,Language.RUSSIAN);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-//        String fileName = "C:\\Users\\SmelayaPanda\\IdeaProjects\\YandexTranslate\\src\\main\\resources\\countRating2.txt";
-//        try {
-//            TxtFileReader.readFile(fileName, list);
-//        } catch (IOException e) {
-//            System.out.println("Hz что произошло");
-//        }
-//        int i = 104490;
-//        for (WordUnit l : list) {
-//            try {
-//                String eng = l.getEng();
-//                Translate.setKey(ApiKeys.YANDEX_API_KEY);
-//                String rus = Translate.execute(eng, Language.ENGLISH, Language.RUSSIAN);
-//                System.out.println(eng + " " + rus + " " + l.getOrdinar());
-//
-//                if (!rus.equals(eng)) {
-//                    WriteNewTxt.writeWithTranslate(eng, rus, i);
-//                    i++;
-//                }
-//            } catch (Exception e) {
-//                System.out.println("tr error");
-//            }
-//        }
+        String fileName = "C:\\Users\\SmelayaPanda\\IdeaProjects\\YandexTranslate\\src\\main\\resources\\countRating2.txt";
+        try {
+            TxtFileReader.readFile(fileName, list);
+        } catch (IOException e) {
+            System.out.println("Hz что произошло");
+        }
+        int i = 104490;
+        for (WordUnit l : list) {
+            try {
+                String eng = l.getEng();
+                Translate.setKey(ApiKeys.YANDEX_API_KEY);
+                String rus = Translate.execute(eng, Language.ENGLISH, Language.RUSSIAN);
+                System.out.println(eng + " " + rus + " " + l.getOrdinar());
+
+                if (!rus.equals(eng)) {
+                    WriteNewTxt.writeWithTranslate(eng, rus, i);
+                    i++;
+                }
+            } catch (Exception e) {
+                System.out.println("tr error");
+            }
+        }
 
     }
 }
