5e3ef5eb373f13720369ed0362d132dba3bf4731
SmelayaPanda <smelayapanda@mail.ru>
2016-05-25 00:18:03 +0600

    350 000 word handling

--- src/main/java/start/StartYandexTranslate.java
+++ src/main/java/start/StartYandexTranslate.java
@@ -1,10 +1,8 @@
 package start;
 
-import ytrans.ApiKeys;
 import ytrans.Language;
 import ytrans.Translate;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,29 +12,34 @@ public class StartYandexTranslate {
 
         List<WordUnit> list = new ArrayList();
 
-
-        String fileName = "C:\\Users\\SmelayaPanda\\IdeaProjects\\YandexTranslate\\src\\main\\resources\\countRating.txt";
         try {
-            TxtFileReader.readFile(fileName, list);
-        } catch (IOException e) {
-            System.out.println("Hz что произошло");
+            Translate.execute("from", Language.ENGLISH,Language.RUSSIAN);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        int i = 1;
-        for (WordUnit l : list) {
-            try {
-                String eng = l.getEng();
-                Translate.setKey(ApiKeys.YANDEX_API_KEY);
-                String rus = Translate.execute(eng, Language.ENGLISH, Language.RUSSIAN);
-                System.out.println(eng + " " + rus + " " + l.getOrdinar());
 
-                if (!rus.equals(eng)) {
-                    WriteNewTxt.writeWithTranslate(eng, rus, i);
-                    i++;
-                }
-            } catch (Exception e) {
-                System.out.println("tr error");
-            }
-        }
+//        String fileName = "C:\\Users\\SmelayaPanda\\IdeaProjects\\YandexTranslate\\src\\main\\resources\\countRating2.txt";
+//        try {
+//            TxtFileReader.readFile(fileName, list);
+//        } catch (IOException e) {
+//            System.out.println("Hz что произошло");
+//        }
+//        int i = 104490;
+//        for (WordUnit l : list) {
+//            try {
+//                String eng = l.getEng();
+//                Translate.setKey(ApiKeys.YANDEX_API_KEY);
+//                String rus = Translate.execute(eng, Language.ENGLISH, Language.RUSSIAN);
+//                System.out.println(eng + " " + rus + " " + l.getOrdinar());
+//
+//                if (!rus.equals(eng)) {
+//                    WriteNewTxt.writeWithTranslate(eng, rus, i);
+//                    i++;
+//                }
+//            } catch (Exception e) {
+//                System.out.println("tr error");
+//            }
+//        }
 
     }
 }
--- src/main/java/ytrans/ApiKeys.java
+++ src/main/java/ytrans/ApiKeys.java
@@ -1,5 +1,5 @@
 package ytrans;
 
 public class ApiKeys {
-  public static final String YANDEX_API_KEY = "trnsl.1.1.20160524T015306Z.5e53bfc8746cd52e.7bdd8dd6cd630fe3dfc3c4da4eec870a0b2da5a3";
+  public static final String YANDEX_API_KEY = "trnsl.1.1.20160524T180534Z.596077343e94785d.13e9d31da5a6956026ea984a5ab6a6960b610d75";
 }
