f97d902b83357e6972f6f3c901fff92fbf222d8e
panda <SmelayaPanda@mail.ru>
2017-12-03 10:24:45 +0700

    for avac

--- src/main/java/start/StartYandexTranslate.java
+++ src/main/java/start/StartYandexTranslate.java
@@ -16,14 +16,14 @@ public class StartYandexTranslate {
     public static void main(String[] args) throws IOException {
 
         Translate.setKey(ApiKeys.YANDEX_API_KEY);
-        TreeMap map = FileReader.readFileToMap("./src/main/resources/forAvac/engWordsWikiArranged.txt");
-        PrintWriter pw = new PrintWriter(new FileWriter("./src/main/resources/forAvac/translatedEngWiki.txt"));
+        TreeMap map = FileReader.readFileToMap("./src/main/resources/forAvac/rusWordsWikiArranged.txt");
+        PrintWriter pw = new PrintWriter(new FileWriter("./src/main/resources/forAvac/translatedRusWikiToEng.txt"));
         AtomicInteger counter = new AtomicInteger();
         map.forEach((key, val) -> {
             counter.getAndIncrement();
             String to = null;
             try {
-                to = Translate.execute((String) val, Language.ENGLISH, Language.RUSSIAN);
+                to = Translate.execute((String) val, Language.RUSSIAN, Language.ENGLISH);
             } catch (Exception e) {
                 e.printStackTrace();
             }
