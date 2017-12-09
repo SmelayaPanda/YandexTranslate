8538e2b28809acec5b0a46bc7a5f70c29fe35b07
panda <SmelayaPanda@mail.ru>
2017-12-09 17:21:43 +0700

    for avac
    Signed-off-by: panda <SmelayaPanda@mail.ru>

--- src/main/java/start/StartYandexTranslate.java
+++ src/main/java/start/StartYandexTranslate.java
@@ -9,7 +9,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.TreeMap;
-import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class StartYandexTranslate {
@@ -18,18 +17,20 @@ public class StartYandexTranslate {
         Translate.setKey(ApiKeys.YANDEX_API_KEY);
         TreeMap map = FileReader.readFileToMap("./src/main/resources/forAvac/spaWordsWikiArranged.txt");
         PrintWriter pw = new PrintWriter(new FileWriter("./src/main/resources/forAvac/translatedSpaWikiToRus.txt"));
-        AtomicInteger counter = new AtomicInteger();
-        map.forEach((key, val) -> {
-            counter.getAndIncrement();
+        final int[] counter = {0};
+        map.forEach((Object key, Object val) -> {
+            counter[0]++;
             String to = null;
-            try {
-                to = Translate.execute((String) val, Language.SPANISH, Language.RUSSIAN);
-            } catch (Exception e) {
-                e.printStackTrace();
+            if (counter[0] > 109323) {
+                try {
+                    to = Translate.execute((String) val, Language.SPANISH, Language.RUSSIAN);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                System.out.println(counter[0] + " --> " + val + " = " + to);
+                pw.write(val + ": " + to + "\n");
+                pw.flush();
             }
-            System.out.println(counter + " --> " + val + " = " + to);
-            pw.write(val + ": " + to + "\n");
-            pw.flush();
         });
 
         pw.close();
